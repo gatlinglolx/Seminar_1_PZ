49450395371691a1de249e314ada35b3f83444d1
vladislav shakhov <shahov199@gmail.com>
2022-12-16 08:32:10 +0300

    Zadacha 2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter number1");
+int number1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter number2");
+int number2 = Convert.ToInt16(Console.ReadLine());
+if (number1 > number2) 
+{
+    Console.Write("max=");
+    System.Console.WriteLine (number1);
+}    
+if (number2 > number1) 
+{
+    Console.Write("max=");
+    System.Console.WriteLine (number2);
+}    
+if (number1 == number2) Console.WriteLine("Числа одинаковые");
+
