75f5579d7c18d897c3dc2e4e13ac87cd0a271ecc
vladislav shakhov <shahov199@gmail.com>
2022-12-16 08:58:32 +0300

    Zadacha 8

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter N");
+int N = Convert.ToInt16(Console.ReadLine());
+int index = 1;
+for (int i = index; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
