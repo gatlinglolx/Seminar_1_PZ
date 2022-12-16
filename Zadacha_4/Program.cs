f07bfd8d1c4e15ef1aa0b355afd51b6ad5167658
vladislav shakhov <shahov199@gmail.com>
2022-12-16 08:38:50 +0300

    Zadacha 4

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter number1");
+int number1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter number2");
+int number2 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter number3");
+int number3 = Convert.ToInt16(Console.ReadLine());
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+System.Console.WriteLine (max);
